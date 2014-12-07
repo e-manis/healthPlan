46d56e35efff6c7aeb4849a9f9d190ea9c8e62dc
shrink <schmittrink@gmail.com>
2014-12-07 23:48:12 +0100

    first shot for a fully working auto deploy

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -3,10 +3,14 @@
 GH_REPO="@github.com/e-manis/healthPlan.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
-echo "post install test"
-git checkout gh-pages
-cp -rf app/pub/* .
-rm -rf app/
-
-git status
-
+# clone the gh-pages branch
+git clone --branch gh-pages ${FULL_REPO} deploy
+# copy the build contents
+cd deploy
+rm -rf *
+cp -rf ../app/pub/* .
+# commit back to github
+REV=$(git rev-parse HEAD)
+git add -A .
+git commit --allow-empty -m "Travis-ci build for commit: $REV"
+git push origin gh-pages
