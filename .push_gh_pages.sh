924306549cdcdb4489df181be60810b60405f316
shrink <schmittrink@gmail.com>
2014-12-07 23:52:42 +0100

    next try

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -3,6 +3,9 @@
 GH_REPO="@github.com/e-manis/healthPlan.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
+# tell git who we are
+git config user.name "e-manis"
+git config user.email "travis"
 # clone the gh-pages branch
 git clone --branch gh-pages ${FULL_REPO} deploy
 # copy the build contents
