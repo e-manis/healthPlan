3899d4a27b371efe2e3f3e0d3818aad7e4820bf6
shrink <schmittrink@gmail.com>
2014-12-08 00:27:48 +0100

    finalized push_gh_pages script

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# set global script vars used to access the repository
 GH_REPO="@github.com/e-manis/healthPlan.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
-
 # clone the gh-pages branch
 git clone --branch gh-pages ${FULL_REPO} deploy
 # tell git who we are
@@ -14,7 +14,6 @@ rm -rf *
 cp -rf ../app/pub/* .
 # commit back to github
 REV=$(git rev-parse HEAD)
-ls -la
 git add -A .
 git commit --allow-empty -m "Travis-ci build for commit: $REV"
 git push origin gh-pages
