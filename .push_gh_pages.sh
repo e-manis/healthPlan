1e03f281f14212ae3d80b9aa996922c310b17ea4
shrink <schmittrink@gmail.com>
2014-12-08 00:12:25 +0100

    stil trying to figure the gh_push auth

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -7,13 +7,14 @@ FULL_REPO="https://$GH_TOKEN$GH_REPO"
 git clone --branch gh-pages ${FULL_REPO} deploy
 # tell git who we are
 git config user.name "e-manis"
-git config user.email "travis"
+git config user.email "e-manis@gmx.de"
 # copy the build contents
 cd deploy
 rm -rf *
 cp -rf ../app/pub/* .
 # commit back to github
 REV=$(git rev-parse HEAD)
+ls -la
 git add -A .
 git commit --allow-empty -m "Travis-ci build for commit: $REV"
 git push origin gh-pages
