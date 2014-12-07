f6b4b22dee7ef9e354c3f460ee62742108928dd5
shrink <schmittrink@gmail.com>
2014-12-07 23:03:49 +0100

    iterating on post-build travis hook

--- push_gh_pages.sh
+++ push_gh_pages.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -rf out || exit 0;
-mkdir out;
-
 GH_REPO="@github.com/e-manis/healthPlan.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
-git config user.name "e-manis-travis"
-git config user.email "travis"
-git clone $FULL_REPO
+echo "post install test"
+git checkout gh-pages
+cp -rf app/pub/* .
+rm -rf app/
+
+git status
 
